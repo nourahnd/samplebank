bc5e8c19512fd6494062daed0f154712abedf79b
nourahnd <nourahasheminejad@gmail.com>
2026-07-13 00:13:24 +0330

    set viper to load config from .env file and implement this feature

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/nourahnd/samplebank/api"
 	db "github.com/nourahnd/samplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/samplebank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/nourahnd/samplebank/utils"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalf("cannot connect to db:%v", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
